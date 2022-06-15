7296ff7b658ba78a4c16bd6d8bfe5a65629d5347
blogchik <blogchikuz@gmail.com>
2022-06-15 18:12:34 +0500

    Edited

--- web/header.php
+++ web/header.php
@@ -7,16 +7,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Official web-site team SafBir. Uzbekistan cyber security team!">
+    <meta name="description" content="Official web-site team SafBir. Uzbekistan cyber developers team!">
     <meta name="keywords" content="SafBir, safbir, uzbekistan, portfolio, web, html, cyber security, security, cyber, developer, coder, business">
-    <meta name="author" content="BlogChik">
+    <meta name="author" content="SafBir Team">
 
     <link rel="shortcut icon" href="./images/SafBir-Logo-Icon.ico" type="image/x-icon">
 
     <link rel="stylesheet" href="./style/main.css">
     <link rel="stylesheet" href="./style/icons.css">
 
-    <title>SafBir - WebSite</title>
+    <title>SafBir - Uzbekistan cyber develoeprs team!</title>
 
 </head>
 
